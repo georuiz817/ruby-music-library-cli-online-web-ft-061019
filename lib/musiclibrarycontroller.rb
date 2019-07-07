65040ce344076b0430c3e5e578fb618d5c4d7c55
George Ruiz <georuiz817@gmail.com>
2019-07-07 23:42:25 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -51,8 +51,8 @@ class MusicLibraryController
   end
 
   def list_genres
-    Genre.all.sort{ |a, b| a.name <=> b.name}.each.with_index(1) do |g, i|
-      puts "#{i}. #{g.name}"
+    Genre.all.sort{ |a, b| a.name <=> b.name}.each.with_index(1) do |c, d|
+      puts "#{d}. #{c.name}"
     end
   end
 
